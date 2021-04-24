f5e61d6b0d812250517cc6ca7411a9b493d8dfda
emaliemcmahon <emaliemcmahon@gmail.com>
2021-04-24 18:21:48 +0000

    updating input size

--- train.py
+++ train.py
@@ -43,8 +43,8 @@ print('==> Preparing data..')
 # image size = 32x32x32
 
 transform_train = transforms.Compose([
-    transforms.Resize(224),
-    transforms.RandomCrop(32, padding=4),
+    transforms.Resize(256),
+    transforms.RandomCrop(224, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
