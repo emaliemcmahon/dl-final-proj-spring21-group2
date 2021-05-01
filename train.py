cfb37fccd31fcd738df3a7d8a7b3fa9403548c0c
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2021-05-01 15:36:33 -0400

    pick up stopped training

--- train.py
+++ train.py
@@ -116,8 +116,6 @@ def train(device, args, trainloader, n_batches, testloader, model, scaler, loss,
             train_acc_epoch += (predicted.float() == label_batch.float()).sum()
             if i == 0 or i % 250 == 0 or i == (n_batches-1):
                 print('For epoch %i, batch %i train loss is %f' % (epoch, i, train_loss_batch.float()))
-            if i == 10:
-                break
 
         total_loss_train.append(train_loss_epoch/train_total)
         total_acc_train.append(float(train_acc_epoch/train_total))
